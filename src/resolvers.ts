e7c5fc498dd23e3b149c16ca7a1057fb2febfd14
bboystatix <statix231@hotmail.com>
2019-12-06 11:37:51 +0800

    Declare broadcastClick argument types

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -6,13 +6,12 @@ const CLICK_BROADCAST = 'CLICK_BROADCAST';
 module.exports = {
     Subscription: {
         clickBroadcast: {
-            // Additional event labels can be passed to asyncIterator creation
             subscribe: () => pubsub.asyncIterator([CLICK_BROADCAST]),
         },
     },
     Mutation: {
-        broadcastClick: async (root:any, args:{[key:string]:string}, context:any) => {
-            pubsub.publish(CLICK_BROADCAST, { clickBroadcast: args })
+        broadcastClick: async (_:any, {timestamp, type}:{[key:string]:string}, __:any) => {
+            pubsub.publish(CLICK_BROADCAST, { clickBroadcast: {timestamp, type} })
             return { success: true, message: 'Broadcast successful!' }
         },
     }
